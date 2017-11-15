d5db4cd0d2717cdaa28d318b181cf2cbe4ea25f9
Max Zhao <alcasa.mz@gmail.com>
2017-11-16 00:55:25 +0100

    adding cross validation

--- learn.py
+++ learn.py
@@ -1,27 +1,34 @@
-import os
 import pandas
+import numpy as np
 from sklearn import svm
 from sklearn.utils import shuffle
 
-os.chdir('/home/max/DREAM')
-data = pandas.read_csv('matrix_output.csv', delimiter=';')
 
-clf = svm.SVC(gamma=0.001, C=1000, kernel='rbf')
+def split_frame(df, splits=5):
+    return np.array_split(df.iloc[:,:-1], splits), np.array_split(df['label'], splits)
 
-def svm_predict(df):
-    pure_data = df.iloc[:,:-1]
-    # labels = data.iloc[:,-1:]
-    labels = df['label']
-    clf.fit(pure_data[:-10], labels[:-10])
-    truth = labels[-10:]
-    p = clf.predict(pure_data[-10:])
-    acc = 0;
-    for i,s in enumerate(truth):
-        if p[i] == s:
-            acc += 1
-    acc = acc / len(truth)
-    print(acc)
-    print(truth)
+def svm_predict(clf, data, labels, splits=5):
+    scores = []
+    for k in range(splits):
+        x_train = list(data)
+        x_test = x_train.pop(k)
+        x_train = np.concatenate(x_train)
+        y_train = list(labels)
+        y_test = y_train.pop(k)
+        y_train = np.concatenate(y_train)
+        scores.append(clf.fit(x_train, y_train).score(x_test, y_test))
+    return scores
 
-df = shuffle(data)
-svm_predict(df)
+def main():
+    data_csv = 'matrix_output.csv'
+    splits = 10
+
+    dataframe = pandas.read_csv(data_csv, delimiter=';')
+    datas, labels = split_frame(dataframe, splits=splits)
+
+    clf = svm.SVC(gamma=0.001, C=100, kernel='linear')
+    scores = svm_predict(clf, datas, labels, splits=splits)
+    print(scores)
+
+if __name__ == '__main__':
+    main()
