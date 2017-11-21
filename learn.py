aee84c7ec2bd4793866579e2031e6aed9a27af09
Max Zhao <alcasa.mz@gmail.com>
2017-11-21 15:42:14 +0100

    implementing parameter search and better test-train split

--- learn.py
+++ learn.py
@@ -2,10 +2,14 @@ import pandas
 import numpy as np
 from sklearn import svm
 from sklearn.utils import shuffle
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV, train_test_split
 
 
-def split_frame(df, splits=5):
+def split_frame_cross(df, splits=5):
     return np.array_split(df.iloc[:,:-1], splits), np.array_split(df['label'], splits)
+def split_frame(df):
+    return df.iloc[:,:-1], df['label']
 
 def svm_predict(clf, data, labels, splits=5):
     scores = []
@@ -19,16 +23,41 @@ def svm_predict(clf, data, labels, splits=5):
         scores.append(clf.fit(x_train, y_train).score(x_test, y_test))
     return scores
 
+def scale_datas(data):
+    data -= data.min()
+    data /= data.max()
+    return data
+
 def main():
-    data_csv = 'matrix_output.csv'
+    data_csv = '/home/max/DREAM/matrix_output_logic_trans_no_lin.csv'
     splits = 10
 
     dataframe = pandas.read_csv(data_csv, delimiter=';')
-    datas, labels = split_frame(dataframe, splits=splits)
+    # datas, labels = split_frame_cross(dataframe, splits=splits)
+    datas, labels = split_frame(dataframe)
+    datas = scale_datas(datas)
+    X_train, X_test, y_train, y_test = train_test_split(
+            datas, labels, test_size=0.5, random_state=0)
 
-    clf = svm.SVC(gamma=0.001, C=100, kernel='linear')
-    scores = svm_predict(clf, datas, labels, splits=splits)
-    print(scores)
+    params = {
+            'C' : list(range(1,1000, 1))
+            ,"gamma" : [0.1, 0.01, 0.001]
+            ,'kernel' : ['linear', 'rbf', 'poly']
+            }
+    grid_search = GridSearchCV(svm.SVC(), params, n_jobs=8)
+    grid_search.fit(X_train, y_train)
+    print(grid_search.best_params_)
+    # print("Grid scores on development set:\n\n")
+    # means = grid_search.cv_results_['mean_test_score']
+    # stds = grid_search.cv_results_['std_test_score']
+    # for mean, std, params in zip(means, stds, grid_search.cv_results_['params']):
+    #     print("%0.3f (+/-%0.03f) for %r"
+    #           % (mean, std * 2, params))
+    y_pred = grid_search.predict(X_test)
+    print(classification_report(y_test, y_pred))
+
+    # scores = svm_predict(clf, datas, labels, splits=splits)
+    # print(scores)
 
 if __name__ == '__main__':
     main()
