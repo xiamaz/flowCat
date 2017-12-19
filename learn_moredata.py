ab5119ec7be6b3ef4aeceaf7593e9b74800387b5
Max Zhao <alcasa.mz@gmail.com>
2017-12-19 21:10:27 +0100

    adding balanced evaluation

--- learn_moredata.py
+++ learn_moredata.py
@@ -1,3 +1,7 @@
+import matplotlib
+import matplotlib
+matplotlib.use('Agg')
+matplotlib.use('Agg')
 import os
 import pandas
 import numpy as np
@@ -47,8 +51,8 @@ def data_histograms(df,groups,k):
     seaborn.set()
     num = df.shape[1] - 2
     x1 = np.array(range(0,num))
-    if not os.path.exists('plots/{}'.format(k)):
-        os.makedirs('plots/{}'.format(k))
+    if not os.path.exists('plots_new/{}'.format(k)):
+        os.makedirs('plots_new/{}'.format(k))
     for g in groups:
         d = df[df['group'] == g]
         data, group, label = d.iloc[:,:-2], d['group'], d['label']
@@ -56,7 +60,7 @@ def data_histograms(df,groups,k):
         for i in range(0,data.shape[0]):
             y = np.array(data.iloc[i,:],dtype='float')
             plt.plot(x1, y)
-        plt.savefig('plots/{}/{}.png'.format(k,g))
+        plt.savefig('plots_new/{}/{}.png'.format(k,g))
 
 def binary_processing(dfs, groups, meta):
     if not os.path.exists('logs/{}'.format(meta)):
@@ -75,38 +79,59 @@ def binary_processing(dfs, groups, meta):
             print("Skip {} entire group because of small size {}".format(g, normals.shape[0]),file=outfile)
             continue
         for kk, df in groups.items():
-            if (df.shape[0] < normals.shape[0] / 2):
-                print("Skip {} because of size {}\n".format(kk, df.shape[0]), file=outfile)
+            if df.shape[0] < 20:
+                print("Skip {} entire group because of small size {}".format(g, normals.shape[0]),file=outfile)
                 continue
-            result = run_optimization(pandas.concat([df, normals]))
+            t1 = shuffle(normals, random_state=0, n_samples=min(normals.shape[0],df.shape[0]))
+            t2 = shuffle(df, random_state=0, n_samples=min(normals.shape[0],df.shape[0]))
+            result = run_optimization(pandas.concat([t1, t2]))
             result_dict['precision'].loc[g,kk] = result[0]
             result_dict['recall'].loc[g,kk] = result[1]
             result_dict['f1'].loc[g,kk] = result[2]
         outfile.close()
     return result_dict
 
+def group_bar_plot(df,k):
+    occ = df.groupby('group').size()
+    plt.figure()
+    ax = seaborn.barplot(x=occ.index,y=occ)
+    ax.set(xlabel='Groups', ylabel='Number of files')
+    loc,labels = plt.xticks()
+    ax.set_xticklabels(labels,rotation=30)
+    plt.savefig('plots_new/num_overview_{}.png'.format(k), dpi=300)
+
 
 def main():
     csvs = {
-            'norm1':'/home/max/DREAM/flowCat/1_MOREDATA_SET1_all_histos.csv'
-            ,'meta1':'/home/max/DREAM/flowCat/1_MOREDATA_SET1_all_metas.csv'
-            ,'norm2':'/home/max/DREAM/flowCat/2_MOREDATA_SET2_all_histos.csv'
-            ,'meta2':'/home/max/DREAM/flowCat/2_MOREDATA_SET2_all_metas.csv'
+            'norm1':'/home/max/DREAM/flowCat/4_MOREDATA_SET1_all_histos.csv'
+            ,'meta1':'/home/max/DREAM/flowCat/4_MOREDATA_SET1_all_metas.csv'
+            ,'norm2':'/home/max/DREAM/flowCat/4_MOREDATA_SET2_all_histos.csv'
+            ,'meta2':'/home/max/DREAM/flowCat/4_MOREDATA_SET2_all_metas.csv'
             }
 
     if not os.path.exists('logs'):
         os.mkdir('logs')
+    if not os.path.exists('plots_new'):
+        os.mkdir('plots_new')
+
     dataframes = { k:pandas.read_csv(csv, delimiter=';') for k,csv in csvs.items() }
     all_groups = { k:v['group'].unique() for k,v in dataframes.items() }
+    [ group_bar_plot(v,k) for k,v in dataframes.items() ]
+    [ data_histograms(v,all_groups[k],k) for k,v in dataframes.items() ]
+    plt.close('all')
 
 
     results = {k:binary_processing(v,all_groups[k],k) for k,v in dataframes.items()}
     for k,v in results.items():
         for kk,vv in v.items():
             plt.figure()
+            plt.subplots(figsize=(10,8))
             plt.title("{} {} heatmap".format(k,kk))
-            seaborn.heatmap(vv)
-            plt.savefig('plots/heatmap_{}_{}.png'.format(k,kk))
+            mask = np.zeros_like(vv)
+            mask[np.triu_indices_from(mask)] = True
+            seaborn.heatmap(vv, cmap='YlGnBu', annot=True, fmt='.2f',mask=mask)
+            plt.savefig('plots_new/heatmap_{}_{}.png'.format(k,kk))
+        plt.close('all')
 
-__name__ == '__main__':
+if __name__ == '__main__':
     main()
