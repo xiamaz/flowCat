b0d07675b0c4fd69d4d84d20dc4fb326696d7b71
Max Zhao <alcasa.mz@gmail.com>
2018-03-12 15:05:13 +0100

    Updating classification script for refactoring

--- classify.py
+++ classify.py
@@ -13,10 +13,10 @@ def joined_tubes(files: [str], name: str):
     data = UpsamplingData.from_file(files[0])
     # data.select_groups(["CLL", "normal", "CLLPL", "HZL",
     #                     "Mantel", "Marginal", "MBL"])
-    clas = Classifier(data)
+    clas = Classifier(data, name=name)
     # clas.holdout_validation(name="joined", ratio=0.9)
     # clas.absolute_validation(name="joined")
-    clas.k_fold_validation(name=name, k_num=5)
+    clas.k_fold_validation(k_num=5)
 
 
 def separate_tubes(files: [str], name: str):
@@ -27,11 +27,11 @@ def separate_tubes(files: [str], name: str):
                         "Mantel", "Marginal", "MBL"])
     # data.exclude_small_cohorts(cutoff=50)
     data.limit_size_to_smallest()
-    clas = Classifier(data)
+    clas = Classifier(data, name=name)
 
     # clas.holdout_validation(name="merged_all", ratio=0.9)
     # clas.absolute_validation(name="plain")
-    clas.k_fold_validation(name=name, k_num=5)
+    clas.k_fold_validation(k_num=5)
 
 
 def main():
@@ -41,18 +41,18 @@ def main():
         "/data/ssdraid/Genetik/PlainNormal_1_output/native_tube1.csv",
         "/data/ssdraid/Genetik/PlainNormal_1_output/native_tube2.csv"
     ]
-    files_margin_all = [
-        "/data/ssdraid/Genetik/MarginUpperLower_1_output/native_tube1.csv",
-        "/data/ssdraid/Genetik/MarginUpperLower_1_output/native_tube2.csv"
-    ]
-    files_margin_upper = [
-        "/data/ssdraid/Genetik/MarginUpper_1_output/native_tube1.csv",
-        "/data/ssdraid/Genetik/MarginUpper_1_output/native_tube2.csv",
-    ]
-    files_margin_lower = [
-        "/data/ssdraid/Genetik/MarginLower_1_output/native_tube1.csv",
-        "/data/ssdraid/Genetik/MarginLower_1_output/native_tube2.csv",
-    ]
+    # files_margin_all = [
+    #     "/data/ssdraid/Genetik/MarginUpperLower_1_output/native_tube1.csv",
+    #     "/data/ssdraid/Genetik/MarginUpperLower_1_output/native_tube2.csv"
+    # ]
+    # files_margin_upper = [
+    #     "/data/ssdraid/Genetik/MarginUpper_1_output/native_tube1.csv",
+    #     "/data/ssdraid/Genetik/MarginUpper_1_output/native_tube2.csv",
+    # ]
+    # files_margin_lower = [
+    #     "/data/ssdraid/Genetik/MarginLower_1_output/native_tube1.csv",
+    #     "/data/ssdraid/Genetik/MarginLower_1_output/native_tube2.csv",
+    # ]
     files_joined = [
         "/data/ssdraid/Genetik/joined/joined_all.csv"
     ]
@@ -60,7 +60,7 @@ def main():
     # separate_tubes(files_margin_all, "margin_all")
     # separate_tubes(files_margin_upper, "margin_upper")
     # separate_tubes(files_margin_lower, "margin_lower")
-    # joined_tubes(files_joined, "joined")
+    joined_tubes(files_joined, "joined")
 
 
 if __name__ == '__main__':
