190b49f682e633303b5696c71cbad07a94af2a0b
Max Zhao <alcasa.mz@gmail.com>
2018-03-21 11:52:23 +0100

    Adaptation of upsampling as executable script

--- create_upsampling.R
+++ create_upsampling.R
@@ -9,8 +9,7 @@ source("lib/fsom.R")
 
 threads.option <- make_option(c("--threads", "-t"), type = "numeric", default = 1,
                               help = "Number of threads", metavar = "threads")
-dataset.option <- make_option(c("--dataset", "-d"), action = "store_true", default = F,
-                              metavar = "dataset", help = "Read single dataset.")
+dataset.option <- make_option(c("--dataset", "-d"), action = "store_true", default = F, help = "Read single dataset.")
 inputpath.option <- make_option(c("--input", "-i"), type = "character",
                                 default = "../Moredata", help = "Input directory", metavar = "input")
 outputpath.option <- make_option(c("--output", "-o"), type = "character",
@@ -19,7 +18,7 @@ groupsize.option <- make_option(c("--groupsize", "-s"), type = "numeric",
                                 default = 20, help = "Threshold group size for fSOM generation.",
                                 metavar = "groupsize")
 
-option.list <- list(threads.option, inputpath.option, outputpath.option, groupsize.option)
+option.list <- list(threads.option, inputpath.option, dataset.option, outputpath.option, groupsize.option)
 
 parser <- OptionParser(usage = "%prog [options] run.name run.number", option_list = option.list)
 arguments <- parse_args(parser, positional_arguments = 2)
@@ -39,25 +38,28 @@ kOutputPath <- file.path(parsed.options$output, sprintf("%d_%s", kRunNumber, kRu
 
 # group size for inclusion in the flowSOM
 kThresholdGroupSize <- parsed.options$groupsize
+
+# selectors for filtering of cases
 kMaterialSelection <- c("1", "2", "3", "4", "5", "PB", "KM")
+kGroupSelection <- c("MBL", "normal")
 
-print(kOutputPath)
-stop()
+# general filters to all files
+filters <- list(material = kMaterialSelection, group = kGroupSelection)
 
+data.reader <- ifelse(parsed.options$dataset, ReadDataset, ReadDatasets)
+all.files <- data.reader(kPath, thread.num = kThreads, filters = filters)
+
+# CREATE output directory
 # annoying by design to prevent overwriting previous results
 if (dir.exists(kOutputPath)){
   stop(paste(kOutputPath, "already exists. Move or delete it."))
 }
 dir.create(kOutputPath, recursive = T, showWarnings = T)
 
-all.files <- ifelse(parsed.options$dataset,
-                    ReadDatasets(kPath, thread.num = kThreads, material = kMaterialSelection),
-                    ReadDataset(kPath, thread.num = kThreads, material = kMaterialSelection))
-
 tube.matrix.1 <- CasesToMatrix(all.files, kThreads, filters = list(tube_set = c(1)),
-                               output.dir = kOutputPath, name = "tube1", sample.size = 10)#, load.func = loader)
+                               output.dir = kOutputPath, name = "tube1", sample.size = kThresholdGroupSize)
 SaveMatrix(kOutputPath, "tube1.csv", tube.matrix.1)
 
 tube.matrix.2 <- CasesToMatrix(all.files, kThreads, filters = list(tube_set = c(2)),
-                               output.dir = kOutputPath, name = "tube2", sample.size = 10)#, load.func = loader)
+                               output.dir = kOutputPath, name = "tube2", sample.size = kThresholdGroupSize)
 SaveMatrix(kOutputPath, "tube2.csv", tube.matrix.2)
