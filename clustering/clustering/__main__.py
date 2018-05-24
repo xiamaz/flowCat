eb569c152ed33c0f1a5418884800df10a1b2c4be
Max Zhao <alcasa.mz@gmail.com>
2018-05-24 07:58:39 +0000

    Fixing stamping bug and specify tubes with cmd args

--- clustering/clustering/__main__.py
+++ clustering/clustering/__main__.py
@@ -23,6 +23,7 @@ parser = ArgumentParser(
 )
 parser.add_argument("-o", "--output", help="Output file directory")
 parser.add_argument("--refcases", help="Json with list of reference cases.")
+parser.add_argument("--tubes", help="Selected tubes.")
 parser.add_argument("-i", "--input", help="Input case json file.")
 parser.add_argument("-t", "--temp", help="Temp path for file caching.")
 parser.add_argument("--bucketname", help="S3 Bucket with data.")
@@ -41,7 +42,11 @@ if args.refcases:
             case for cases in json.load(reffile).values() for case in cases
         ]
 
-for tube in cases.tubes:
+outdir = "{}_{}".format(args.output, create_stamp())
+
+tubes = map(int, args.tubes.split(";")) if args.tubes else cases.tubes
+
+for tube in tubes:
     if refcases:
         data = cases.get_train_from_case_labels(refcases, tube=tube)
     else:
@@ -51,7 +56,7 @@ for tube in cases.tubes:
     results = []
     labels = []
     groups = []
-    for label, group, testdata in cases.get_all_data(num=300, tube=tube):
+    for label, group, testdata in cases.get_all_data(num=2000, tube=tube):
         print("Upsampling {}".format(label))
         results.append(pipe.transform(testdata))
         labels.append(label)
@@ -63,6 +68,5 @@ for tube in cases.tubes:
     def writefun(dest):
         df_all.to_csv(dest, sep=";")
 
-    outdir = "{}_{}".format(args.output, create_stamp())
     outpath = os.path.join(outdir, "tube{}.csv".format(tube))
     put_file_path(outpath, writefun, args.temp)
