2c24dfe94dd4ff3bdd873930cd87f102c17f9cfd
Max Zhao <alcasa.mz@gmail.com>
2018-06-11 19:20:05 +0200

    Scripts for adding new batch experiments

---
+++ aws/comp_pregating.fish
@@ -0,0 +1,12 @@
+#!/usr/bin/fish
+
+source run_batch.fish
+
+# Create experiments for indiv pregating
+set TAG "comp_pregating"
+
+set RAND 5
+
+set EXPERIMENTS "pregated_combined" "somgated" "som"
+
+submit_jobs $TAG $RAND $EXPERIMENTS
