a9dab940dccdec1c3c7dbb3392fba826c21f47f0
Max Zhao <alcasa.mz@gmail.com>
2018-07-12 10:49:57 +0200

    Cleaning plotting functions

--- clustering/clustering/plotting.py
+++ clustering/clustering/plotting.py
@@ -32,8 +32,8 @@ def plot_histogram(data, ax, title=None):
     ax.plot(mean_data.index, mean_data, "k-")
     ax.fill_between(
         pd.to_numeric(mean_data.index),
-        (mean_data-std_data).clip_lower(0),
-        (mean_data+std_data).clip_upper(1.0)
+        (mean_data - std_data).clip_lower(0),
+        (mean_data + std_data).clip_upper(1.0)
     )
     ax.set_ylim(0, 1.0)
     ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
@@ -58,7 +58,7 @@ def plot_2d(ax, channels, data, labels=None, dotsize=1):
 def plot_history(fig, history, title=""):
     num_plots = len(history)
     # calculate number of rows for plotting depending on plotwidth
-    num_rows = int((num_plots-1) / MAX_PLOTWIDTH) + 1
+    num_rows = int((num_plots - 1) / MAX_PLOTWIDTH) + 1
 
     fig.set_size_inches(
         MAX_PLOTWIDTH * PLOTWIDTH,
@@ -110,7 +110,7 @@ def plot_overview(
     w = viewnum if viewnum < MAX_PLOTWIDTH else MAX_PLOTWIDTH
 
     if not fig:
-        fig = Figure(dpi=100, figsize=(4*w, 4*h))
+        fig = Figure(dpi=100, figsize=(4 * w, 4 * h))
     if gs and gspos is not None:
         overview_grid = GridSpecFromSubplotSpec(
             2, 1, subplot_spec=gs[gspos], height_ratios=[1, 7]
@@ -142,7 +142,7 @@ def plot_overview(
     dims = [0, 0.03, 1, 0.95]
     if not inner_grid:
         fig.tight_layout(rect=dims)
-    return fig, (w*PLOTWIDTH, h*PLOTHEIGHT)
+    return fig, (w * PLOTWIDTH, h * PLOTHEIGHT)
 
 
 def plot_upsampling(fig, data, title="Upsampling"):
@@ -151,51 +151,9 @@ def plot_upsampling(fig, data, title="Upsampling"):
         cdata = gdata.drop("infiltration", axis=1)
         mean_data = cdata.mean()
         std_data = cdata.std()
-        ax = fig.add_subplot(5, 2, i+1)
+        ax = fig.add_subplot(5, 2, i + 1)
         plot_histogram((mean_data, std_data), ax, title=group)
     fig.suptitle(title)
     fig.set_size_inches(20, 10)
     fig.tight_layout(rect=(0, 0, 1, 0.95))
     return fig
-
-
-def get_umatrix(weights, m, n):
-    """ Generates an n x m u-matrix of the SOM's weights.
-
-    Used to visualize higher-dimensional data. Shows the average distance
-    between a SOM unit and its neighbors.  When displayed, areas of a darker
-    color separated by lighter colors correspond to clusters of units which
-    encode similar information.
-    :param weights: SOM weight matrix, `ndarray`
-    :param m: Rows of neurons
-    :param n: Columns of neurons
-    :return: m x n u-matrix `ndarray`
-    """
-    umatrix = np.zeros((m * n, 1))
-    # Get the location of the neurons on the map to figure out their neighbors.
-    # I know I already have this in the SOM code but I put it here too to make
-    # it easier to follow.
-    neuron_locs = list()
-    for i in range(m):
-        for j in range(n):
-            neuron_locs.append(np.array([i, j]))
-    # Get the map distance between each neuron (i.e. not the weight distance).
-    neuron_distmat = distance_matrix(neuron_locs, neuron_locs)
-
-    for i in range(m * n):
-        # Get the indices of the units which neighbor i
-        neighbor_idxs = neuron_distmat[i] <= 1  # Change this to `< 2` if you want to include diagonal neighbors
-        # Get the weights of those units
-        neighbor_weights = weights[neighbor_idxs]
-        # Get the average distance between unit i and all of its neighbors
-        # Expand dims to broadcast to each of the neighbors
-        umatrix[i] = distance_matrix(np.expand_dims(weights[i], 0), neighbor_weights).mean()
-
-    return umatrix.reshape((m, n))
-
-
-def plot_weights(weights, m, n):
-    umatrix = get_umatrix(weights, m, n)
-    fig = plt.figure()
-    plt.imshow(umatrix, origin='lower')
-    plt.show(block=True)
