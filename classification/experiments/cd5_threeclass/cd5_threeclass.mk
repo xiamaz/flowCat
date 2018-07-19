176159b803a0c65326d4bc4d0de879bf7309afcf
Max Zhao <alcasa.mz@gmail.com>
2018-07-20 00:13:34 +0200

    Adding cd5 dependent groups for classification

---
+++ classification/experiments/cd5_threeclass/cd5_threeclass.mk
@@ -0,0 +1,8 @@
+TAG := $(basename $(notdir $(lastword $(MAKEFILE_LIST))))_
+
+GROUPS = CD5pos:CLL,MBL,Mantel,CLLPL;CD5neg:FL,LPL,Marginal;normal
+METHOD = kfold:10
+FILTERS = max_size:2000
+TUBES = 1;2
+
+PATTERN := initial_comp_normal_2018
