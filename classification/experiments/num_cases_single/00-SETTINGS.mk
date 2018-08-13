8f4f41321dbbdc635bef58bece4c816c2e7f1b2f
Max Zhao <alcasa.mz@gmail.com>
2018-08-13 12:37:23 +0200

    Adding cohort number experiments

---
+++ classification/experiments/num_cases_single/00-SETTINGS.mk
@@ -0,0 +1,8 @@
+TAG := $(shell basename $(dir $(lastword $(MAKEFILE_LIST))))
+METHOD = kfold:10
+TUBES = 1;2
+
+GROUPS = --group "HZL;FL;LPL;Marginal;Mantel;CLLPL;CLL;MBL;normal"
+SIZE = --size 3000
+
+SET = num_cases
---
+++ classification/experiments/num_cases_single/c25.mk
@@ -0,0 +1,2 @@
+NOTE = Consensus SOM with 25 cases from each cohort.
+PATTERN = c25
