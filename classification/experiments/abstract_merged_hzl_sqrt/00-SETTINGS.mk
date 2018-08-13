a1f689faa43e44ede6e3d39c4b060d7484c9f3e3
Max Zhao <alcasa.mz@gmail.com>
2018-08-13 11:11:16 +0200

    Adding new merged experiments

---
+++ classification/experiments/abstract_merged_hzl_sqrt/00-SETTINGS.mk
@@ -0,0 +1,9 @@
+TAG := $(shell basename $(dir $(lastword $(MAKEFILE_LIST))))
+METHOD = kfold:10
+TUBES = 1;2
+
+GROUPS = --group "LMg:LPL,Marginal;MtCp:Mantel,CLLPL;CM:CLL,MBL;FL;HZL;normal"
+SIZE = --size 3000
+
+SET = abstract
+EXTRA_ARGS = --transform sqrt
