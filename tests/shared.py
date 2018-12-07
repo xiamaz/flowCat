50d2e9b176985f869d7db460d78f83d2c37b5e49
Alcasa <alcasa.mz@gmail.com>
2018-12-07 12:02:16 +0100

    Adding unitest for read SOM (#18)

--- tests/shared.py
+++ tests/shared.py
@@ -3,6 +3,8 @@ Shared functions used by all tests.
 """
 import os
 import pathlib
+import numpy as np
+import pandas as pd
 import flowcat.utils as utils
 
 # Path to test directory containing test files
@@ -18,3 +20,32 @@ HISTO_PATH = DATAPATH / "histogram" / "abstract_normal"
 
 # set utils tmp path to another location
 utils.TMP_PATH = "tmp_test"
+
+
+class MockLoader:
+    """Return mock data for different path calls."""
+
+
+    URLPath = utils.URLPath
+
+
+    def load_csv(self, path):
+        path = str(path)
+        if path == "som9":
+            # get a regular matrix from 0 to 89 in 9 rows with 10 values each
+            data = pd.DataFrame(np.arange(90).reshape(9, 10))
+        elif path == "som4":
+            data = pd.DataFrame(np.array([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]]))
+        elif path == "somcount4":
+            data = pd.DataFrame(np.array([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]]))
+            data["counts"] = np.ones(4) * 90
+        elif path == "sombothcount4":
+            data = pd.DataFrame(np.array([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]]))
+            data["counts"] = np.ones(4) * 90
+            data["count_prev"] = np.ones(4) * 100
+        elif path == "somprevcount4":
+            data = pd.DataFrame(np.array([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]]))
+            data["count_prev"] = np.ones(4) * 100
+        else:
+            raise KeyError(f"No mock data available for {path}")
+        return data
