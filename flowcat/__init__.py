0003519a8b86226d2e051200a155cc1009f9fd13
Max Zhao <alcasa.mz@gmail.com>
2019-05-06 10:18:40 +0200

    Fixing import

--- flowcat/__init__.py
+++ flowcat/__init__.py
@@ -3,4 +3,4 @@ from .dataset.case_dataset import CaseCollection
 from .som import create_som, load_som, SOM, SOMCollection
 from .models.tfsom import SOMNodes, TFSom, FCSSomTransformer
 from .utils import create_handler, add_logger
-import utils
+from . import utils
