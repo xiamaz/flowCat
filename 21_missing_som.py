b35873e88e3ac50fc137c5703b295f7303f9a8c7
Max Zhao <alcasa.mz@gmail.com>
2019-05-14 16:52:05 +0200

    use data from samples

--- 21_missing_som.py
+++ 21_missing_som.py
@@ -14,30 +14,29 @@ def configure_print_logging(rootname="flowcat"):
     flowcat.utils.add_logger(rootname, handlers, level=logging.DEBUG)
 
 
-def train_native():
-    ds_missing = flowcat.CaseCollection.from_path("output/missing")
-    ds_subsample = flowcat.CaseCollection.from_path("output/subsample")
-
-    tube = 1
-    markers = ds_missing.get_markers(tube)
-    markers_list = markers.index.values
-
-    filepath = ds_missing.data[0].get_tube(tube)
-    fcsdata = filepath.data
+def train_native(sample, outname):
+    fcsdata = sample.data
 
     sample_size = 5000
 
     model = flowcat.models.FCSSom(
-        (32, 32, -1), markers=markers_list,
-        batch_size=500,
-        tensorboard_dir="output/21-tensorboard/native")
+        (32, 32, -1), markers=sample.markers,
+        batch_size=500, tube=sample.tube,
+        tensorboard_dir=f"output/21-tensorboard/{outname}")
     model.train([fcsdata], sample=sample_size)
 
     weights = model.weights
 
-    flowcat.save_som(weights, "output/21-tfsom/native")
+    flowcat.save_som(weights, f"output/21-tfsom/{outname}")
 
 
 if __name__ == "__main__":
     configure_print_logging()
-    train_native()
+
+    ds_missing = flowcat.CaseCollection.from_path("output/missing")
+    sample_missing = ds_missing.data[0].get_tube(1)
+    train_native(sample_missing, "native-missing")
+
+    ds_subsample = flowcat.CaseCollection.from_path("output/subsample")
+    sample_subsample = ds_subsample.data[0].get_tube(1)
+    train_native(sample_subsample, "native-subsample")
