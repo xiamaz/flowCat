661bfd55986afb4c1c627fd4a72c60ffb1a2a8f0
Max Zhao <alcasa.mz@gmail.com>
2019-07-09 16:13:18 +0200

    Adding processing script for bonn dataset

---
+++ 00_bonn_dataset.py
@@ -0,0 +1,175 @@
+"""
+Overview of cases dataset:
+
+- number of samples
+"""
+import time
+import contextlib
+from argparse import ArgumentParser
+import collections
+
+import flowcat
+
+
+@contextlib.contextmanager
+def timing(label):
+    time_a = time.time()
+    yield
+    time_b = time.time()
+    time_d = time_b - time_a
+    print(f"{label} time: {time_d}s")
+
+
+@contextlib.contextmanager
+def block(label):
+    print(label)
+    yield
+    print("-" * len(label))
+
+
+def deduplicate_cases_by_sureness(cases):
+    """Remove duplicates by taking the one with the higher sureness score."""
+    label_dict = collections.defaultdict(list)
+    for case in cases:
+        label_dict[case.id].append(case)
+    deduplicated = []
+    duplicates = []
+    for same_id_cases in label_dict.values():
+        same_id_cases.sort(key=lambda c: c.sureness, reverse=True)
+        if len(same_id_cases) == 1 or same_id_cases[0].sureness > same_id_cases[1].sureness:
+            deduplicated.append(same_id_cases[0])
+        else:
+            duplicates.append(same_id_cases[0].id)
+            print(
+                "DUP both removed: %s (%s), %s (%s)" % (
+                    same_id_cases[0].id, same_id_cases[0].group, same_id_cases[1].id, same_id_cases[1].group
+                ))
+    if duplicates:
+        print("%d duplicates removed" % len(duplicates))
+    deduplicated_cases = flowcat.CaseCollection(deduplicated, **cases.config)
+    deduplicated_cases.add_filter_step({"custom_filter": "deduplicate_cases_by_sureness"})
+    return deduplicated_cases
+
+
+def get_selected_markers(cases, tubes, marker_threshold=0.9):
+    """Get a list of marker channels available in all tubes."""
+    selected_markers = {}
+    for tube in tubes:
+        marker_counts = collections.Counter(
+            marker for t in [case.get_tube(tube) for case in cases]
+            if t is not None for marker in t.markers)
+        # get ratio of availability vs all cases
+        selected_markers[tube] = [
+            marker for marker, count in marker_counts.items()
+            if count / len(cases) > marker_threshold and "nix" not in marker
+        ]
+    print(selected_markers)
+    selected_cases, _ = cases.filter_reasons(selected_markers=selected_markers)
+    selected_cases.selected_markers = selected_markers
+    return selected_cases
+
+
+def print_diff(cases_a, cases_b):
+    """Print differences in number of cases in a and number of cases in b."""
+    num_a = len(cases_a)
+    num_b = len(cases_b)
+    diff_total = abs(num_a - num_b)
+    print(f"Total: {num_a} → {num_b} -- |{diff_total}|")
+
+    group_a = cases_a.group_count
+    group_b = cases_b.group_count
+    for key, value_a in group_a.items():
+        value_b = group_b[key]
+        diff_key = value_a - value_b
+        print(f"{key}: {value_a} → {value_b} -- |{diff_key}|")
+
+
+def preprocess_cases(cases: flowcat.CaseCollection, tubes=(1, 2, 3)):
+    with block("Deduplicate cases by sureness"):
+        deduplicated = deduplicate_cases_by_sureness(cases)
+        print_diff(cases, deduplicated)
+        cases = deduplicated
+
+    with block("Filter cases on selected markers"):
+        selected_cases = get_selected_markers(cases, tubes)
+        print_diff(cases, selected_cases)
+        cases = selected_cases
+        print("Markers selected are:")
+        print("\n".join(f"{k}: {v}" for k, v in cases.selected_markers.items()))
+
+    with block("Filter train"):
+        train_cases, _ = cases.filter_reasons(
+            date=(None, "2018-06-30"),
+            materials=flowcat.ALLOWED_MATERIALS,
+            tubes=(1, 2, 3))
+        print_diff(cases, train_cases)
+
+    with block("Filter test"):
+        test_cases, _ = cases.filter_reasons(
+            date=("2018-07-01", None),
+            materials=flowcat.ALLOWED_MATERIALS,
+            tubes=(1, 2, 3))
+        print_diff(cases, test_cases)
+
+    train_labels = train_cases.labels
+    test_labels = test_cases.labels
+    train_not_test = all(t not in test_labels for t in train_labels)
+    test_not_train = all(t not in train_labels for t in test_labels)
+    assert train_not_test and test_not_train
+
+    print("Ratio", len(test_cases) / (len(train_cases) + len(test_cases)))
+
+    with timing("Filter all"):
+        all_cases, reasons = cases.filter_reasons(
+            materials=flowcat.ALLOWED_MATERIALS,
+            tubes=(1, 2, 3))
+        print_diff(cases, all_cases)
+        reason_count = collections.Counter([r for _, rs in reasons for r in rs])
+        print("Reasons are", ", ".join(f"{k}: {v}" for k, v in reason_count.items()))
+
+    newest = None
+    for case in all_cases:
+        if not newest:
+            newest = case
+        elif newest.date < case.date:
+            newest = case
+    print("Newest case", newest)
+
+    return train_cases, test_cases
+
+
+def export_filtered(
+        train_cases: flowcat.CaseCollection,
+        test_cases: flowcat.CaseCollection,
+        output: flowcat.utils.URLPath):
+    """Export metadata after filtering to the given output directory."""
+    flowcat.utils.save_json(train_cases.json, output / "train.json")
+    flowcat.utils.save_json(train_cases.config, output / "train_config.json")
+    flowcat.utils.save_json(test_cases.json, output / "test.json")
+    flowcat.utils.save_json(test_cases.config, output / "test_config.json")
+
+
+def main(args):
+    cases = flowcat.CaseCollection.from_path(args.path)
+    print(cases)
+    selected = get_selected_markers(cases, (1, 2), marker_threshold=0.9)
+    selected, _ = selected.filter_reasons(groups=["normal", "CLL"])
+    flowcat.utils.save_json(selected.json, args.output / "bonn.json")
+    flowcat.utils.save_json(selected.config, args.output / "bonn_config.json")
+
+
+if __name__ == "__main__":
+    PARSER = ArgumentParser(
+        description="Get overview of available data and create a dataset used for SOM transformation."
+    )
+    PARSER.add_argument(
+        "path",
+        type=flowcat.utils.URLPath,
+        help="Path to dataset",
+    )
+    PARSER.add_argument(
+        "-o", "--output", type=flowcat.utils.URLPath,
+        help="Path to save output metadata",
+        default=flowcat.utils.URLPath("output/00-dataset-test")
+    )
+    main(PARSER.parse_args())
