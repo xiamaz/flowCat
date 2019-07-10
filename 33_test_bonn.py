881249d1748bea5e813d0ffce92d1bc2d1537310
Max Zhao <alcasa.mz@gmail.com>
2019-07-10 16:50:50 +0200

    Changing test location

--- 33_test_bonn.py
+++ 33_test_bonn.py
@@ -16,16 +16,16 @@ def load_model(path):
 def main(args):
     dataset = flowcat.SOMDataset.from_path(args.input)
 
+    dataseq = flowcat.SOMSequence(dataset, binarizer, tube=1)
+    preds = binarizer.inverse_transform(model.predict_generator(dataseq))
+    result = dataset.labels == preds
+    print("Untrained accuracy:", sum(result) / len(result))
+
     train, test = dataset.split(ratio=0.8)
     print(train, test)
     print(train.group_counts)
     print(test.group_counts)
     binarizer, model = load_model(args.model)
-
-    dataseq = flowcat.SOMSequence(dataset, binarizer, tube=1)
-    preds = binarizer.inverse_transform(model.predict_generator(dataseq))
-    result = dataset.labels == preds
-    print("Untrained accuracy:", sum(result) / len(result))
 
     trainseq = flowcat.SOMSequence(train, binarizer, tube=1)
     testseq = flowcat.SOMSequence(test, binarizer, tube=1)
