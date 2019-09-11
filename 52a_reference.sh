06f73da8e53252ac32a1a99990bed84f81ff13e4
Max Zhao <alcasa.mz@gmail.com>
2019-09-11 21:39:55 +0200

    Adding scripts to generate reference and soms for berlin data

---
+++ 52a_reference.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./01a_create_ref_som.py --data /data/flowcat-data/lb-data2 --meta output/5-berlin-data/dataset/known_groups.json output/5-berlin-data/dataset/references.json output/5-berlin-data/reference
---
+++ 52b_soms.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./01b_create_soms.py --data /data/flowcat-data/lb-data2 --meta output/5-berlin-data/dataset/known_groups.json output/5-berlin-data/reference output/5-berlin-data/soms
