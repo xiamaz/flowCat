Fix SOM generation and implement masking
Mask SOM output based on whether the data point exists
in the fcs data. The weight updates and distance calculations
then only consider the existing values.

In order to enable masking the FCS class now
also contains a mask array, which is of the same
shape as the fcs data.

Squashed commit of the following:

commit a21fbf31a679e7cbaedc66bde79107886b9d7b15
Author: Max Zhao <alcasa.mz@gmail.com>
Date:   Fri Sep 20 17:26:27 2019 +0200

    Removing learning rate references in config

commit a438108a741041ab10f8451efa9478f5412fe495
Author: Max Zhao <alcasa.mz@gmail.com>
Date:   Thu Sep 19 19:16:08 2019 +0200

    Fixing fcssom training

commit 9a1ad599fb13ebff0f95d5e4ba226c554a4203d6
Author: Max Zhao <alcasa.mz@gmail.com>
Date:   Thu Sep 19 18:45:14 2019 +0200

    Fixing scalers to use np array based fcs objects

commit 3e6c78bdcf9e114578bae2422c1af9e8d2c827a4
Author: Max Zhao <alcasa.mz@gmail.com>
Date:   Wed Sep 18 23:14:21 2019 +0200

    Adding tests for fcs and tfsom

commit efe667130e08d4ecd08422d91a3740a720fb4b80
Author: Max Zhao <alcasa.mz@gmail.com>
Date:   Wed Sep 18 18:34:00 2019 +0200

    Adding tfsom fixes

flowcat/dataset/fcs.py
@@ -8,9 +8,8 @@ import logging
 from collections import namedtuple
 
 import numpy as np
-import pandas as pd
 
-import fcsparser
+from fcsparser.api import FCSParser
 
 from flowcat.utils import URLPath, outer_interval
 from flowcat.mappings import MARKER_NAME_MAP
@@ -34,29 +33,26 @@ def extract_name(marker):
     return name
 
 
-def create_meta_from_data(data: pd.DataFrame) -> dict:
-    """Get min max ranges from pandas dataframe."""
+def create_meta_from_data(data: np.array, channels: list) -> dict:
+    """Get min max ranges from numpy array."""
     min_values = data.min(axis=0)
     max_values = data.max(axis=0)
-    channel_metas = {}
-    for name, min_value in min_values.items():
-        max_value = max_values[name]
-        channel_metas[name] = ChannelMeta(
-            pd.Interval(min_value, max_value, closed="both"),
-            True)
+    channel_metas = {
+        name: ChannelMeta(min_value, max_value, (0, 0), 0)
+        for name, min_value, max_value in zip(channels, min_values, max_values)
+    }
     return channel_metas
 
 
-def create_meta_from_fcs(meta: dict, data: pd.DataFrame) -> dict:
+def create_meta_from_fcs(meta: dict, channels: list) -> dict:
     """Get ranges from pnr in metadata."""
     channel_metas = {
         c: ChannelMeta(
-            pd.Interval(0, int(meta[f"$P{i + 1}R"]), closed="both"),
-            True,
+            0, int(meta[f"$P{i + 1}R"]),
             tuple(map(float, meta[f"$P{i + 1}E"].split(","))),
             float(meta[f"$P{i + 1}G"]),
         )
-        for i, c in enumerate(data.columns)
+        for i, c in enumerate(channels)
     }
     return channel_metas
 
@@ -70,35 +66,28 @@ def join_fcs_data(fcs_data: List[FCSData], channels=None) -> FCSData:
     Returns:
         Merged single FCSData containing entries from all given fcsdata files.
     """
+    # outer join our data
     if channels is None:
-        channels = fcs_data[0].channels
+        channels = list(set(c for d in fcs_data for c in d.channels))
 
-    dfs = [data.align(channels).data for data in fcs_data]
-    joined = pd.concat(dfs)
-
-    all_meta = [data.meta for data in fcs_data]
-    meta = functools.reduce(
-        lambda x, y: {
-            name: ChannelMeta(
-                outer_interval(x[name].range, y[name].range),
-                x[name].exists or y[name].exists
-            ) for name in channels
-        }, all_meta
-    )
-
-    return FCSData(joined, meta=meta)
+    aligned = [data.align(channels) for data in fcs_data]
+    data = np.concatenate([a.data for a in aligned])
+    mask = np.concatenate([a.mask for a in aligned])
+    new_data = FCSData((data, mask), channels=channels)
+    return new_data
 
 
-ChannelMeta = namedtuple("ChannelMeta", field_names=["range", "exists", "pne", "png"])
+ChannelMeta = namedtuple("ChannelMeta", field_names=["min", "max", "pne", "png"])
 
 
 class FCSData:
     """Wrap FCS data with additional metadata"""
 
     __slots__ = (
-        "_data",  # pd dataframe of data
-        "_meta",  # dict of channel name to channel meta namedtuples
-        "_channels",  # channel names
+        "data",  # pd dataframe of data
+        "mask",  # np array mask for data
+        "meta",  # dict of channel name to channel meta namedtuples
+        "channels",  # channel names
     )
 
     default_encoding = "latin-1"
@@ -106,8 +95,9 @@ class FCSData:
 
     def __init__(
             self,
-            initdata: Union[pd.DataFrame, URLPath, FCSData],
-            meta: dict = None):
+            initdata: Union[URLPath, FCSData, tuple],
+            meta: dict = None,
+            channels: list = None,):
         """Create a new FCS object.
 
         Args:
@@ -118,114 +108,89 @@ class FCSData:
         """
         if isinstance(initdata, self.__class__):
             if meta is not None:
-                raise NotImplementedError("Meta not used for urlpath fcs objects.")
+                raise NotImplementedError("Meta not used for fcs object data input.")
 
-            self._data = initdata.data.copy()
-            self._meta = initdata.meta.copy()
+            self.data = initdata.data.copy()
+            self.mask = initdata.mask.copy()
+            self.meta = initdata.meta.copy()
+            self.channels = initdata.channels.copy()
         elif isinstance(initdata, URLPath):
             if meta is not None:
-                raise NotImplementedError("Meta not used for urlpath fcs objects.")
+                raise NotImplementedError("Meta not used for urlpath data input.")
 
-            meta, data = fcsparser.parse(
-                str(initdata), data_set=self.default_dataset, encoding=self.default_encoding)
-            self._data = data
-            self._meta = create_meta_from_fcs(meta, data)
-        elif isinstance(initdata, pd.DataFrame):
-            self._data = initdata
-            self._meta = meta or create_meta_from_data(initdata)
+            # meta, data = fcsparser.parse(
+            #     str(initdata), data_set=self.default_dataset, encoding=self.default_encoding)
+            parser = FCSParser(str(initdata), data_set=self.default_dataset, encoding=self.default_encoding)
+            self.data = parser.data
+            self.mask = np.ones(self.data.shape)
+            self.meta = create_meta_from_fcs(parser.annotation, parser.channel_names_s)
+            self.channels = list(parser.channel_names_s)
+        elif isinstance(initdata, tuple):
+            self.data, self.mask = initdata
+            if channels is None:
+                raise ValueError("Channels needed when initializing from np data")
+            self.meta = meta or create_meta_from_data(self.data, channels)
+            self.channels = channels
         else:
             raise RuntimeError(
                 "Invalid data for FCS. Either Path, similar object or tuple of data and metadata needed.")
 
-        self._data = self._data.astype("float32", copy=False)
-        self._channels = None
+        self.data = self.data.astype("float32", copy=False)
 
     @property
     def shape(self):
-        return self._data.shape
+        return self.data.shape
 
     @property
-    def channels(self) -> List[str]:
-        if self._channels is None:
-            self._channels = list(self._data.columns)
-        return self._channels
+    def ranges_array(self):
+        """Get min max ranges as numpy array."""
+        return np.array([
+            [meta.min, meta.max] for meta in self.meta.values()
+        ]).T
 
-    @property
-    def data(self) -> pd.DataFrame:
-        return self._data
-
-    @property
-    def meta(self) -> dict:
-        return self._meta
-
-    @property
-    def ranges_dataframe(self) -> pd.DataFrame:
-        """Return min and max range as dataframe."""
-        ranges_dict = {
-            n: {"min": m.range.left, "max": m.range.right}
-            for n, m in self.meta.items()
-        }
-        ranges = pd.DataFrame(data=ranges_dict, dtype="float32")
-        return ranges
-
-    def set_data(self, data):
-        if set(list(data.columns)) != set(self.channels):
-            raise FCSException("Channels are different in new data.")
-        self._data = data
-
-    def set_ranges_from_dataframe(self, ranges: pd.DataFrame):
-        """Set ranges from ranges dataframe with optional info on closedness."""
-        ranges = {
-            name: pd.Interval(
-                min_max["min"], min_max["max"],
-                self.meta[name].range.closed
-            ) for name, min_max in ranges.to_dict().items()
-        }
-        self.set_ranges(ranges)
-
-    def set_ranges(self, ranges: dict):
-        """Set meta ranges from dict mapping channel names to intervals."""
-        for name, channel_range in ranges.items():
-            self.set_range(name, channel_range)
-
-    def set_range(self, name, channel_range: pd.Interval):
-        self.meta[name] = ChannelMeta(channel_range, self.meta[name].exists)
+    def update_range(self, range_array):
+        for name, col in zip(self.meta, range_array.T):
+            self.meta[name] = self.meta[name]._replace(min=col[0], max=col[1])
 
     def rename(self, mapping: dict):
         """Rename columns based on the given mapping."""
-        self._data.rename(mapping, axis=1, inplace=True)
-        self._meta = {mapping.get(name, name) for name, meta in self.meta.items()}
-        self._channels = None
+        self.channels = [mapping.get(name, name) for name in self.channels]
+        self.meta = {mapping.get(name, name): data for name, data in self.meta.items()}
 
     def marker_to_name_only(self) -> FCSData:
         mapping = {c: extract_name(c) for c in self.channels}
         self.rename(mapping)
         return self
 
-    def add_missing_columns(self, channels: List[str], missing_val=np.nan) -> FCSData:
+    def reorder_channels(self, channels: List[str]) -> FCSData:
+        """Reorder columns based on list of channels given."""
+        if any(map(lambda c: c not in self.channels, channels)):
+            raise ValueError("Some given channels not contained in data.")
+        index = np.array([self.channels.index(c) for c in channels])
+        self.data = self.data[:, index]
+        self.mask = self.mask[:, index]
+        self.channels = channels
+
+    def add_missing_channels(self, channels: List[str]) -> FCSData:
         """Add missing columns in the given channel list to the dataframe and
         set them to the missing value."""
+        if any(map(lambda c: c in self.channels, channels)):
+            raise ValueError("Given channel already in data.")
         cur_channels = self.channels
-        missing_cols = {k: missing_val for k in channels if k not in cur_channels}
-        if len(missing_cols) / float(len(channels)) > 0.5:
-            LOGGER.warning("More %d columns are missing. Maybe something is wrong.", len(missing_cols))
+        new_channels = cur_channels + channels
 
-        self._data = self._data.assign(**missing_cols)
-        self._meta.update(
-            {
-                name: ChannelMeta(
-                    pd.Interval(missing_val, missing_val),
-                    False)
-                for name, missing_val in missing_cols.items()
-            }
-        )
-        self._channels = None
+        cur_dim_a, cur_dim_b = self.data.shape
+        new_len = len(channels)
+        newdata = np.zeros((cur_dim_a, cur_dim_b + new_len))
+        newdata[:, :-new_len] = self.data
+        newmask = np.zeros((cur_dim_a, cur_dim_b + new_len))
+        newmask[:, :-new_len] = self.mask
+
+        self.meta.update({name: ChannelMeta(0, 0, (0, 0), 0) for name in channels})
+        self.data = newdata
+        self.mask = newmask
+        self.channels = new_channels
         return self
-
-    def channel_mask(self):
-        """Return a 1/0 int array for the given channels, whether channels
-        exist or not."""
-        return np.array([self.meta[c].exists for c in self.channels])
 
     def align(
             self,
@@ -251,11 +216,15 @@ class FCSData:
         if name_only:
             copy.marker_to_name_only()
 
-        copy.add_missing_columns(channels, missing_val=missing_val)
+        dropped_channels = [c for c in copy.channels if c not in channels]
+        if dropped_channels:
+            copy.drop_channels(dropped_channels)
 
-        copy._data = copy._data[channels]
-        copy._meta = {name: copy._meta[name] for name in channels}
-        copy._channels = channels
+        missing_channels = [c for c in channels if c not in copy.channels]
+        if missing_channels:
+            copy.add_missing_channels(missing_channels)
+
+        copy.reorder_channels(channels)
         return copy
 
     def copy(self) -> FCSData:
@@ -275,10 +244,13 @@ class FCSData:
         Returns:
             self. This operation is done in place, so the original object will be modified!
         """
-        self._data.drop(channels, axis=1, inplace=True, errors="ignore")
+        remaining = [c for c in self.channels if c not in channels]
+        remaining_indices = np.array([self.channels.index(c) for c in remaining])
+        self.data = self.data[:, remaining_indices]
+        self.mask = self.mask[:, remaining_indices]
+        self.channels = remaining
         for channel in channels:
-            del self._meta[channel]
-        self._channels = None
+            del self.meta[channel]
         return self
 
     def __repr__(self):

flowcat/sommodels/fcssom.py
@@ -197,15 +197,18 @@ class FCSSom:
 
         if getattr(self.scaler, "fcsdata_scaler", False):
             joined = self.scaler.fit_transform(joined)
-            res = joined.data
+            arr = joined.data
         else:
-            res = joined.data
-            res = self.scaler.fit_transform(res)
+            arr = joined.data
+            arr = self.scaler.fit_transform(arr)
 
+        mask = joined.mask
         if sample > 0:
-            res = res[np.random.choice(res.shape[0], sample, replace=False), :]
+            selection = np.random.choice(arr.shape[0], sample, replace=False)
+            arr = arr[selection, :]
+            mask = mask[selection, :]
 
-        self.model.train(res)
+        self.model.train(arr, mask)
         self.trained = True
         return self
 
@@ -222,10 +225,13 @@ class FCSSom:
             res = data.data
             res = scaler.transform(res)
 
+        mask = data.mask
         if sample > 0:
-            res = res[np.random.choice(res.shape[0], sample, replace=False), :]
+            selection = np.random.choice(res.shape[0], sample, replace=False)
+            res = res[selection, :]
+            mask = mask[selection, :]
 
-        weights = self.model.transform(res, label=label)
+        weights = self.model.transform(res, mask, label=label)
         somweights = self._create_som(weights)
         return somweights
 

flowcat/sommodels/tfsom.py
@@ -188,7 +188,7 @@ def create_initializer(init, init_data, dims):
 
 def summary_quantization_error(squared_distance):
     """Create quantization error."""
-    mean_distance = tf.sqrt(tf.cast(tf.reduce_min(squared_distance, axis=1), tf.float32))
+    mean_distance = tf.sqrt(tf.reduce_min(squared_distance, axis=1))
     _, update_mean_dist = tf.metrics.mean(mean_distance)
     return tf.summary.scalar('quantization_error', update_mean_dist)
 
@@ -223,7 +223,6 @@ class TFSom:
             dims, initialization=None, graph=None,
             max_epochs=10, batch_size=10000, buffer_size=1_000_000,
             initial_radius=None, end_radius=None, radius_cooling="linear",
-            initial_learning_rate=0.05, end_learning_rate=0.01, learning_cooling="linear",
             node_distance="euclidean", map_type="planar", std_coeff=0.5,
             subsample_size=None,
             model_name="Self-Organizing-Map",
@@ -240,9 +239,6 @@ class TFSom:
             initial_radius: Initial radius of neighborhood function.
             end_radius: End radius of neighborhood function on the last epoch.
             radius_cooling: Decay of radius over epochs.
-            initial_learning_rate: Learning rate at epoch 0.
-            end_learning_rate: Learning rate at last epoch.
-            learning_cooling: Decay type of learning rate over epochs.
             node_distance: Distance metric between nodes on the SOM map.
             map_type: Behavior of map edges. Either toroid (wrap-around) or planar (no wrap).
             std_coeff: Coefficient of the neighborhood function.
@@ -260,10 +256,6 @@ class TFSom:
         self._end_radius = 1.0 if end_radius is None else float(end_radius)
         self._radius_cooling = radius_cooling
 
-        self._initial_learning_rate = initial_learning_rate
-        self._end_learning_rate = end_learning_rate
-        self._learning_cooling = learning_cooling
-
         # node distance calculation option on the SOM map
         self._node_distance = node_distance
         self._map_type = map_type
@@ -280,17 +272,12 @@ class TFSom:
         self._ref_weights = None
         self._epoch = None
 
+        self._data_placeholder = None
+        self._mask_placeholder = None
+
         self._training_op = None
         self._assign_trained_op = None
         self._reset_weights_op = None
-
-        # prediction variables
-        self._invar = None
-        self.__prediction_input = None
-        self._squared_distances = None
-        self._prediction_output = None
-        self._prediction_distance = None
-        self._transform_output = None
 
         self._subsample_size = subsample_size
 
@@ -308,6 +295,7 @@ class TFSom:
                 f.writelines(str(config))
 
         self._summary_list = []
+        self._epoch_start_init_vars = []
 
         # This will be the collection of summaries for this subgraph. Add new
         # summaries to it and pass it to merge()
@@ -371,7 +359,7 @@ class TFSom:
                 log_device_placement=False,))
 
         with self._graph.as_default():
-            self._data_placeholder = self._initialize_tf_graph()
+            self._data_placeholder, self._mask_placeholder = self._initialize_tf_graph()
             self._saver = tf.train.Saver()
 
             # Initalize all variables
@@ -379,8 +367,9 @@ class TFSom:
             self._sess.run([init_op])
 
             # Get some metric variables which we will reset each epoch
-            self._metric_initializers = tf.variables_initializer(
-                self._graph.get_collection(tf.GraphKeys.METRIC_VARIABLES)
+            self._epoch_start_init_vars += self._graph.get_collection(tf.GraphKeys.METRIC_VARIABLES)
+            self._epoch_start_init = tf.variables_initializer(
+                self._epoch_start_init_vars
             )
 
         self._initialized = True
@@ -394,26 +383,38 @@ class TFSom:
 
     def _create_input(self):
         """Create placeholder inputs for a dataset using an reinitializable iterator."""
-        data_placeholder = tf.placeholder(tf.float64)
+        data_placeholder = tf.placeholder(tf.float32)
+        mask_placeholder = tf.placeholder(tf.float32)
 
-        dataset = tf.data.Dataset.from_tensor_slices(data_placeholder)
+        dataset = tf.data.Dataset.from_tensor_slices((data_placeholder, mask_placeholder))
         dataset = dataset.batch(self._batch_size)
         dataset = dataset.shuffle(self._buffer_size, seed=None, reshuffle_each_iteration=True)
 
         iterator = dataset.make_initializable_iterator()
-        input_tensor = iterator.get_next()
-        return data_placeholder, iterator, input_tensor
+
+        return data_placeholder, mask_placeholder, iterator
 
     def _initialize_tf_graph(self):
         """Initialize the SOM on the TensorFlow graph"""
-        data, iter_init, input_tensor = self._create_input()
-        self._data_initializer = iter_init.initializer
+        data, mask, iterator = self._create_input()
+        self._epoch_start_init_vars.append(iterator)
 
         with tf.variable_scope(tf.get_variable_scope()):
             (
                 numerators, denominators,
                 self._epoch, self._weights, _, summaries
-            ) = self._tower_som(input_tensor, self._initialization)
+            ) = self._tower_som(iterator, self._initialization)
+
+            sum_numerator = tf.get_variable(
+                "numerator",
+                shape=(self._m * self._n, self._dim),
+                initializer=tf.zeros_initializer())
+            sum_denominator = tf.get_variable(
+                "denominator",
+                shape=(self._m * self._n, self._dim),
+                initializer=tf.zeros_initializer(),
+            )
+            self._epoch_start_init_vars += [sum_numerator, sum_denominator]
 
             self._ref_weights = tf.get_variable(
                 name="ref_weights", initializer=self._weights)
@@ -421,8 +422,13 @@ class TFSom:
             tf.get_variable_scope().reuse_variables()
             self.add_summary(summaries)
 
+            self._batch_op = tf.group(
+                tf.assign_add(sum_numerator, numerators),
+                tf.assign_add(sum_denominator, denominators),
+            )
+
             # Divide them
-            new_weights = tf.divide(numerators, denominators)
+            new_weights = tf.divide(sum_numerator, sum_denominator)
             # diff new and old weights
             if self.tensorboard:
                 diff_weights = tf.reshape(
@@ -436,12 +442,13 @@ class TFSom:
             # Assign them
             with tf.control_dependencies(control_deps):
                 self._training_op = tf.assign(self._weights, new_weights)
+
             self._assign_trained_op = tf.assign(self._ref_weights, self._weights)
             self._reset_weights_op = tf.assign(self._weights, self._ref_weights)
 
-        return data
+        return data, mask
 
-    def _tower_som(self, input_tensor, initialization):
+    def _tower_som(self, iterator, initialization):
         """Build a single SOM tower on the TensorFlow graph
         Args:
             input_tensor: Input event data to be mapped to the SOM should have len(channel) width
@@ -461,24 +468,19 @@ class TFSom:
                 initializer=initializer
             )
 
-        # Matrix of size [m*n, 2] for SOM grid locations of neurons.
-        # Maps an index to an (x,y) coordinate of a neuron in the map for
-        # calculating the neighborhood distance
-        location_vects = tf.constant(np.array(
-            [[i, j] for i in range(self._m) for j in range(self._n)]
-        ), name='Location_Vectors')
-
         with tf.name_scope('Input'):
-            input_copy = tf.cast(input_tensor, tf.float32)
+            input_tensor, mask_tensor = iterator.get_next()
+
             if self._subsample_size:
                 # randomly select samples from the input for training
                 random_vals = tf.cast(
                     tf.transpose(tf.expand_dims(
                         tf.random_uniform(
                             (self._subsample_size, )
-                        ) * tf.cast(tf.shape(input_copy)[0], tf.float32), axis=0)),
+                        ) * tf.cast(tf.shape(input_tensor)[0], tf.float32), axis=0)),
                     tf.int32)
-                input_copy = tf.gather_nd(input_copy, random_vals)
+                input_tensor = tf.gather_nd(input_tensor, random_vals)
+                mask_tensor = tf.gather_nd(mask_tensor, random_vals)
 
         # Feed epoch via feed dict, makes everything much simpler
         with tf.name_scope('Epoch'):
@@ -486,12 +488,16 @@ class TFSom:
 
         # get best matching units for all events in batch
         with tf.name_scope('BMU_Indices'):
-            # squared distance of [batch_size, num_neurons], eg for each event
-            # to all neurons
-            squared_distance = tf.reduce_sum(
-                tf.pow(tf.subtract(tf.expand_dims(weights, axis=0),
-                                   tf.expand_dims(input_copy, axis=1)), 2), 2)
-            # TODO: use sample mask to null some terms
+            # calculate dist for each cell to all nodes for all channels
+            # shape [s, n, c]
+            squared_channel_diffs = tf.pow(
+                tf.subtract(tf.expand_dims(weights, axis=0),  # to num of samples
+                            tf.expand_dims(input_tensor, axis=1)),
+                2)
+            # apply existance mask to distance calcualtion
+            masked_channel_diffs = tf.multiply(
+                squared_channel_diffs, tf.expand_dims(mask_tensor, axis=1), name="masking")
+            squared_distance = tf.reduce_sum(masked_channel_diffs, 2)
 
             bmu_indices = tf.argmin(squared_distance, axis=1, name="map_to_node_index")
 
@@ -506,6 +512,13 @@ class TFSom:
 
         # get the locations of BMU for each event
         with tf.name_scope('BMU_Locations'):
+            # Matrix of size [m*n, 2] for SOM grid locations of neurons.
+            # Maps an index to an (x,y) coordinate of a neuron in the map for
+            # calculating the neighborhood distance
+            location_vects = tf.constant(np.array(
+                [[i, j] for i in range(self._m) for j in range(self._n)]
+            ), name='Location_Vectors')
+
             bmu_locs = tf.reshape(
                 tf.gather(location_vects, bmu_indices), [-1, 2]
             )
@@ -517,10 +530,6 @@ class TFSom:
             radius = apply_cooling(
                 self._radius_cooling,
                 self._initial_radius, self._end_radius,
-                epoch, self._max_epochs)
-            alpha = apply_cooling(
-                self._learning_cooling,
-                self._initial_learning_rate, self._end_learning_rate,
                 epoch, self._max_epochs)
 
             # calculate the node distances between BMU and all other nodes
@@ -541,41 +550,37 @@ class TFSom:
                                 self._std_coeff)),
                         2)))
 
-            # learn rate dependent on neighborhood
-            learning_rate_op = tf.multiply(neighbourhood_func, alpha)
-
         with tf.name_scope('Update_Weights'):
             # weight input with learning rate and sum across events, if we
             # divide with the summed learning rate we will get a distance
             # weighted update
             # shape: [num_neurons, dimensions]
+            masked_learning_rate = tf.multiply(
+                tf.expand_dims(neighbourhood_func, axis=-1),
+                tf.expand_dims(mask_tensor, axis=1),
+            )
             numerator = tf.reduce_sum(
                 tf.multiply(
-                    tf.expand_dims(learning_rate_op, axis=-1),
-                    tf.expand_dims(input_copy, axis=1)
+                    masked_learning_rate,
+                    tf.expand_dims(input_tensor, axis=1)
                 ), axis=0)
 
             # sum neighborhood function, eg the learn rate of each neuron
             # we divide the batch summed new weights through the neighborhood
             # function sum
             # shape: [batch_size, neurons]
-            denominator = tf.expand_dims(
-                tf.reduce_sum(learning_rate_op, axis=0) + float(1e-12),
-                axis=-1)
+            denominator = tf.reduce_sum(masked_learning_rate, axis=0) + float(1e-12)
 
         summaries = []
         if self.tensorboard:
             with tf.name_scope('Summary'):
-                _, update_mean_alpha = tf.metrics.mean(alpha)
-                summaries.append(tf.summary.scalar('alpha', update_mean_alpha))
-
                 _, update_mean_radius = tf.metrics.mean(radius)
                 summaries.append(tf.summary.scalar('radius', update_mean_radius))
 
                 summaries.append(summary_quantization_error(squared_distance))
                 summaries.append(summary_topographic_error(squared_distance, location_vects))
 
-                summaries.append(summary_learning_image(learning_rate_op, self._m, self._n))
+                summaries.append(summary_learning_image(neighbourhood_func, self._m, self._n))
 
             with tf.name_scope("MappingSummary"):
                 event_image = tf.reshape(mapped_events_per_node, shape=(1, self._m, self._n, 1))
@@ -586,15 +591,19 @@ class TFSom:
             weights, mapped_events_per_node, summaries
         )
 
-    def run_till_tensor(self, tensors, data: np.array, epoch: int):
+    def run_till_tensor(self, tensors, data: np.array, mask: np.array, epoch: int):
         assert data.shape[0] <= self._buffer_size, (
             f"Data size {data.shape[0]} > Buffer size {self._buffer_size}. "
             "Samples will be lost on reshuffling. "
             "Increase buffer size to number of samples.")
 
         self._sess.run(
-            [self._data_initializer, self._metric_initializers],
-            feed_dict={self._data_placeholder: data, self._epoch: epoch}
+            self._epoch_start_init,
+            feed_dict={
+                self._data_placeholder: data,
+                self._mask_placeholder: mask,
+                self._epoch: epoch
+            }
         )
         result = self._sess.run(
             tensors,
@@ -602,13 +611,14 @@ class TFSom:
         )
         return result
 
-    def run_till_op(self, op_name: str, data: np.array, epoch: int):
+    def run_till_op(self, op_name: str, data: np.array, mask: np.array, epoch: int):
         operation = self._graph.get_operation_by_name(op_name)
-        return self.run_till_tensor(operation.outputs, data, epoch)
+        return self.run_till_tensor(operation.outputs, data, mask, epoch)
 
     def _run_training(
             self,
             data: np.array,
+            mask: np.array,
             set_weights: bool = False,
             label: str = ""):
         """
@@ -643,14 +653,18 @@ class TFSom:
                 run_metadata = tf.RunMetadata()
 
             self._sess.run(
-                [self._data_initializer, self._metric_initializers],
-                feed_dict={self._data_placeholder: data, self._epoch: epoch}
+                self._epoch_start_init,
+                feed_dict={
+                    self._data_placeholder: data,
+                    self._mask_placeholder: mask,
+                    self._epoch: epoch
+                }
             )
             while True:
                 try:
                     if self.tensorboard:
                         summary, _, = self._sess.run(
-                            [merged_summaries, self._training_op],
+                            [merged_summaries, self._batch_op],
                             options=run_options, run_metadata=run_metadata,
                             feed_dict={self._epoch: epoch}
                         )
@@ -658,7 +672,7 @@ class TFSom:
                         self._writer.add_summary(summary, global_step)
                     else:
                         self._sess.run(
-                            self._training_op,
+                            self._batch_op,
                             feed_dict={self._epoch: epoch}
                         )
                     LOGGER.info("Global step: %d", global_step)
@@ -667,23 +681,28 @@ class TFSom:
                 except tf.errors.OutOfRangeError:
                     break
 
+            # Calculate final weights after all batches have been processed
+            self._sess.run(
+                self._training_op, feed_dict={self._epoch: epoch}
+            )
+
         # set ref_weights to our current weights, these will be used to reset
         # weights the next time run_training is called.
         if set_weights:
             self._sess.run(self._assign_trained_op)
         return self
 
-    def train(self, data, label="learn") -> TFSom:
+    def train(self, data, mask, label="learn") -> TFSom:
         """Train the network on the data provided by the input tensor.
         Args:
             data_iterable: Iterable object returning single pandas dataframes.
         """
-        self._run_training(data, set_weights=True, label=label)
+        self._run_training(data, mask, set_weights=True, label=label)
         return self
 
-    def transform(self, data, label="transform") -> np.array:
+    def transform(self, data, mask, label="transform") -> np.array:
         """Train data using given parameters from initial values transiently."""
-        self._run_training(data, set_weights=False, label=label)
+        self._run_training(data, mask, set_weights=False, label=label)
         return self._sess.run(self._weights)
 
     def save(self, path: URLPath):

tests/test_fcs.py
@@ -1,8 +1,8 @@
 """Test processing of FCS files."""
 import unittest
 
-import pandas as pd
-from pandas.testing import assert_frame_equal
+import numpy as np
+from numpy.testing import assert_array_equal
 import fcsparser
 
 from flowcat.dataset import fcs
@@ -20,34 +20,149 @@ class TestFCS(unittest.TestCase):
         with self.subTest("from another object"):
             first = fcs.FCSData(shared.DATAPATH / "fcs/cll1_1.lmd")
             second = fcs.FCSData(first)
-            assert_frame_equal(first.data, second.data)
+            assert_array_equal(first.data, second.data)
 
-        with self.subTest("from meta data tuple"):
-            meta, data = fcsparser.parse(str(shared.DATAPATH / "fcs/cll1_1.lmd"))
-            meta = fcs.create_meta_from_fcs(meta, data)
-            created = fcs.FCSData(data, meta=meta)
-            assert_frame_equal(created.data, data, check_dtype=False)
+        with self.subTest("from np array"):
+            data = np.random.rand(10, 4)
+            mask = np.ones((10, 4))
+            channels = ["a", "b", "c", "d"]
+            fcs.FCSData((data, mask), channels=channels)
+
+    def test_rename(self):
+        testdata = fcs.FCSData(
+            (np.zeros((10, 4)), np.zeros((10, 4))),
+            channels=["a", "b", "c", "d"]
+        )
+        testdata.rename({"a": "z"})
+        self.assertEqual(testdata.channels, ["z", "b", "c", "d"])
+        self.assertIn("z", testdata.meta)
+        self.assertNotIn("a", testdata.meta)
+
+    def test_marker_to_name_only(self):
+        testdata = fcs.FCSData(
+            (np.zeros((10, 4)), np.zeros((10, 4))),
+            channels=["a-a", "b-b", "c-c", "d-d"]
+        )
+        testdata.marker_to_name_only()
+        self.assertEqual(testdata.channels, ["a", "b", "c", "d"])
+
+    def test_reorder_channels(self):
+        data = np.array([
+            [0, 1, 2],
+            [0, 1, 2],
+            [7, 8, 9],
+        ])
+        testdata = fcs.FCSData(
+            (data, np.ones(data.shape)),
+            channels=["A", "B", "C"]
+        )
+        testdata.reorder_channels(["B", "A", "C"])
+        self.assertEqual(testdata.channels, ["B", "A", "C"])
+        assert_array_equal(testdata.data, np.array([
+            [1, 0, 2],
+            [1, 0, 2],
+            [8, 7, 9],
+        ]))
+        testdata.reorder_channels(["C", "B", "A"])
+        self.assertEqual(testdata.channels, ["C", "B", "A"])
+        assert_array_equal(testdata.data, np.array([
+            [2, 1, 0],
+            [2, 1, 0],
+            [9, 8, 7],
+        ]))
+
+    def test_add_missing(self):
+        data = np.array([
+            [0, 1, 2],
+            [0, 1, 2],
+            [7, 8, 9],
+        ])
+        testdata = fcs.FCSData(
+            (data, np.ones(data.shape)),
+            channels=["A", "B", "C"]
+        )
+        testdata.add_missing_channels(["D"])
+        self.assertEqual(testdata.channels, ["A", "B", "C", "D"])
+        assert_array_equal(testdata.data, np.array([
+            [0, 1, 2, 0],
+            [0, 1, 2, 0],
+            [7, 8, 9, 0],
+        ]))
+        assert_array_equal(testdata.mask, np.array([
+            [1, 1, 1, 0],
+            [1, 1, 1, 0],
+            [1, 1, 1, 0],
+        ]))
+
+        with self.assertRaises(ValueError):
+            testdata.add_missing_channels(["A"])
+
+    def test_drop_channels(self):
+        data = np.array([
+            [0, 1, 2],
+            [0, 1, 2],
+            [7, 8, 9],
+        ])
+        testdata = fcs.FCSData(
+            (data, np.ones(data.shape)),
+            channels=["A", "B", "C"]
+        )
+        testdata.drop_channels(["A"])
+        self.assertEqual(testdata.channels, ["B", "C"])
+        self.assertNotIn("A", testdata.meta)
+        assert_array_equal(testdata.data, np.array([
+            [1, 2],
+            [1, 2],
+            [8, 9],
+        ]))
+
+    def test_align(self):
+        data = np.array([
+            [0, 1, 2],
+            [0, 1, 2],
+            [7, 8, 9],
+        ])
+        testdata = fcs.FCSData(
+            (data, np.ones(data.shape)),
+            channels=["A", "B", "C"]
+        )
+        aligned = testdata.align(["B", "D", "A"])
+        self.assertEqual(aligned.channels, ["B", "D", "A"])
+        assert_array_equal(aligned.data, np.array([
+            [1, 0, 0],
+            [1, 0, 0],
+            [8, 0, 7],
+        ]))
+        assert_array_equal(aligned.mask, np.array([
+            [1, 0, 1],
+            [1, 0, 1],
+            [1, 0, 1],
+        ]))
 
     def test_join(self):
-        first = fcs.FCSData(shared.DATAPATH / "fcs/cll1_1.lmd")
-        second = fcs.FCSData(shared.DATAPATH / "fcs/cll2_1.lmd")
-        joined = fcs.join_fcs_data([first, second])
-        self.assertEqual(joined.shape, (100000, 12))
+        with self.subTest("diff_same_channels"):
+            first = fcs.FCSData(shared.DATAPATH / "fcs/cll1_1.lmd")
+            second = fcs.FCSData(shared.DATAPATH / "fcs/cll2_1.lmd")
+            joined = fcs.join_fcs_data([first, second])
+            self.assertEqual(joined.shape, (100000, 12))
+            self.assertEqual(set(joined.channels), {
+                'FMC7-FITC',
+                'CD23-APC',
+                'FS INT LIN',
+                'CD10-PE',
+                'CD5-PacBlue',
+                'SS INT LIN',
+                'CD20-PC7',
+                'CD45-KrOr',
+                'CD79b-PC5.5',
+                'nix-APCA700',
+                'CD19-APCA750',
+                'IgM-ECD'
+            })
 
-    def test_ranges_df(self):
-        first = fcs.FCSData(shared.DATAPATH / "fcs/cll1_1.lmd")
-        expected = pd.DataFrame({
-            'FS INT LIN': {"max": 1024, "min": 0},
-            'SS INT LIN': {"max": 1024, "min": 0},
-            'FMC7-FITC': {"max": 1024, "min": 0},
-            'CD10-PE': {"max": 1024, "min": 0},
-            'IgM-ECD': {"max": 1024, "min": 0},
-            'CD79b-PC5.5': {"max": 1024, "min": 0},
-            'CD20-PC7': {"max": 1024, "min": 0},
-            'CD23-APC': {"max": 1024, "min": 0},
-            'nix-APCA700': {"max": 1024, "min": 0},
-            'CD19-APCA750': {"max": 1024, "min": 0},
-            'CD5-PacBlue': {"max": 1024, "min": 0},
-            'CD45-KrOr': {"max": 1024, "min": 0},
-        })
-        assert_frame_equal(first.ranges_dataframe, expected)
+        with self.subTest("same_case"):
+            first = fcs.FCSData(shared.DATAPATH / "fcs/cll1_1.lmd")
+            second = fcs.FCSData(shared.DATAPATH / "fcs/cll1_2.lmd")
+            joined = fcs.join_fcs_data([first, second])
+            self.assertEqual(joined.shape, (100000, 19))
+            self.assertEqual(max(np.sum(joined.mask, axis=0)), 100000)

tests/test_fcssom.py
@@ -1,9 +1,7 @@
-import logging
 import unittest
 
 import numpy as np
-import pandas as pd
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_array_almost_equal
 
 from flowcat.dataset import fcs
 from flowcat.sommodels import fcssom
@@ -11,7 +9,7 @@ from flowcat.sommodels import fcssom
 
 SEED = 42
 
-MARKERS = ("AA", "BB")
+MARKERS = ["AA", "BB"]
 
 np.random.seed(SEED)
 
@@ -20,18 +18,31 @@ class FCSSomTestCase(unittest.TestCase):
 
     def test_basic_train(self):
         model = fcssom.FCSSom(
-            (4, 4, 2),
+            (3, 3, 2),
             seed=SEED,
             markers=MARKERS,
         )
 
         traindata = fcs.FCSData(
-            pd.DataFrame(np.random.rand(1000, 2), columns=MARKERS)
+            (np.random.rand(1000, 2), np.ones((1000, 2))),
+            channels=MARKERS,
         )
         testdata = fcs.FCSData(
-            pd.DataFrame(np.random.rand(1000, 2), columns=MARKERS)
+            (np.random.rand(1000, 2), np.ones((1000, 2))),
+            channels=MARKERS,
         )
 
+        expected = np.array([
+            [[0.23379141, 0.81184906],
+             [0.20201758, 0.52560365],
+             [0.18959665, 0.2370348]],
+            [[0.5282842, 0.7789625],
+             [0.5025789, 0.47923133],
+             [0.4661186, 0.19752763]],
+            [[0.7997755, 0.7657486],
+             [0.792592, 0.4714995],
+             [0.763365, 0.18549682]]
+        ])
         model.train([traindata])
         result = model.transform(testdata)
-        print(result.data)
+        assert_array_almost_equal(result.data, expected)

tests/test_scalers.py
@@ -1,7 +1,7 @@
 import unittest
 
-import pandas as pd
-from pandas.testing import assert_frame_equal
+import numpy as np
+from numpy.testing import assert_array_almost_equal
 
 from flowcat.dataset import fcs
 from flowcat.preprocessing import scalers
@@ -18,16 +18,21 @@ class MinMaxScalerTest(unittest.TestCase):
             (
                 "test_a",
                 fcs.FCSData(
-                    pd.DataFrame({"A": [1, 2, 3, 10], "B": [4, 2, 5, 99]}),
+                    (np.array([
+                        [1, 2, 3, 10],
+                        [4, 2, 5, 99]
+                    ]).T, np.ones((4, 2))),
+                    channels=["A", "B"],
                 ),
                 fcs.FCSData(
-                    pd.DataFrame({
-                        "A": [0.0, 0.111111, 0.222222, 1.0],
-                        "B": [0.020619, 0.0, 0.030928, 1.0]
-                    }),
-                    {
-                        "A": fcs.ChannelMeta(pd.Interval(0.0, 1.0, "both"), True),
-                        "B": fcs.ChannelMeta(pd.Interval(0.0, 1.0, "both"), False)
+                    (np.array([
+                        [0.0, 0.111111, 0.222222, 1.0],
+                        [0.020619, 0.0, 0.030928, 1.0]
+                    ]).T, np.ones((4, 2))),
+                    channels=["A", "B"],
+                    meta={
+                        "A": fcs.ChannelMeta(0.0, 1.0, pne=(0, 0), png=0),
+                        "B": fcs.ChannelMeta(0.0, 1.0, pne=(0, 0), png=0),
                     }
                 ),
                 False
@@ -38,29 +43,32 @@ class MinMaxScalerTest(unittest.TestCase):
             with self.subTest(name=name, fit_to_range=fit_to_range):
                 model = scalers.FCSMinMaxScaler(fit_to_range=fit_to_range)
                 result = model.fit_transform(testdata)
-                assert_frame_equal(result.data, expected.data, check_less_precise=True)
-                assert_frame_equal(result.ranges_dataframe, expected.ranges_dataframe, check_less_precise=True)
+                assert_array_almost_equal(result.data, expected.data)
+                assert_array_almost_equal(result.ranges_array, expected.ranges_array)
 
 
 class StandardScalerTest(unittest.TestCase):
 
     def test_basic(self):
-        fit_to_range = False
-
         test_datas = [
             (
                 "test_a",
                 fcs.FCSData(
-                    pd.DataFrame({"A": [1, 2, 3, 10], "B": [4, 2, 5, 99]}),
+                    (np.array([
+                        [1, 2, 3, 10],
+                        [4, 2, 5, 99]
+                    ]).T, np.ones((4, 2))),
+                    channels=["A", "B"],
                 ),
                 fcs.FCSData(
-                    pd.DataFrame({
-                        "A": [-0.84852815, -0.56568545, -0.28284273, 1.6970563],
-                        "B": [-0.56908065, -0.61751306, -0.5448645, 1.7314582]
-                    }),
-                    {
-                        "A": fcs.ChannelMeta(pd.Interval(-0.84852815, 1.6970563, "both"), True),
-                        "B": fcs.ChannelMeta(pd.Interval(-0.61751306, 1.7314582, "both"), False)
+                    (np.array([
+                        [-0.84852815, -0.56568545, -0.28284273, 1.6970563],
+                        [-0.56908065, -0.61751306, -0.5448645, 1.7314582]
+                    ]).T, np.ones((4, 2))),
+                    channels=["A", "B"],
+                    meta={
+                        "A": fcs.ChannelMeta(-0.84852815, 1.6970563, pne=(0, 0), png=0),
+                        "B": fcs.ChannelMeta(-0.61751306, 1.7314582, pne=(0, 0), png=0),
                     }
                 ),
             )
@@ -70,5 +78,5 @@ class StandardScalerTest(unittest.TestCase):
             with self.subTest(name=name):
                 model = scalers.FCSStandardScaler()
                 result = model.fit_transform(testdata)
-                assert_frame_equal(result.data, expected.data, check_less_precise=True)
-                assert_frame_equal(result.ranges_dataframe, expected.ranges_dataframe, check_less_precise=True)
+                assert_array_almost_equal(result.data, expected.data)
+                assert_array_almost_equal(result.ranges_array, expected.ranges_array)

tests/test_tfsom.py
@@ -2,7 +2,7 @@ import logging
 import unittest
 
 import numpy as np
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_array_equal, assert_allclose
 
 from flowcat.sommodels import tfsom
 
@@ -26,8 +26,9 @@ class TFSomTestCase(unittest.TestCase):
         model.initialize()
 
         data = np.random.rand(1000, 4)
+        mask = np.ones((1000, 4))
 
-        model.train(data)
+        model.train(data, mask)
 
         expected = np.array([
             79, 64, 92, 95, 39, 41, 39, 86, 35,
@@ -44,25 +45,54 @@ class TFSomTestCase(unittest.TestCase):
         ])
 
         newdata = np.random.rand(100, 4)
-        mapped, = model.run_till_op("BMU_Indices/map_to_node_index", newdata, 0)
+        newmask = np.ones((100, 4))
+        mapped, = model.run_till_op("BMU_Indices/map_to_node_index", newdata, newmask, 0)
         assert_array_equal(mapped, expected)
+
+    def test_small_batch(self):
+        """Check that different batch sizes do not affect training results."""
+        data = np.random.rand(1000, 4)
+        mask = np.ones((1000, 4))
+
+        model = tfsom.TFSom(
+            (10, 10, 4),
+            seed=SEED,
+            batch_size=1000,
+        )
+        model.initialize()
+        model.train(data, mask)
+        single_batch = model.output_weights
+
+        model = tfsom.TFSom(
+            (10, 10, 4),
+            seed=SEED,
+            batch_size=100,
+        )
+        model.initialize()
+        model.train(data, mask)
+        multi_batch = model.output_weights
+        assert_allclose(single_batch, multi_batch, rtol=1e-04)
 
     def test_missing_data(self):
         model = tfsom.TFSom(
-            (10, 10, 4),
+            (3, 3, 4),
             seed=SEED,
         )
         model.initialize()
 
-        data = np.random.rand(1000, 4)
-        model.train(data)
-
-        newdata = np.random.rand(100, 4)
-        newdata[:, 0] = 0
-        newdata[:, 2] = 0
-        mask = (0, 1, 0, 1)
-        print(newdata)
-        result = model.run_till_op("BMU_Indices/map_to_node_index", newdata, 0)
+        data = np.random.rand(5, 4)
+        mask = np.array([
+            [1, 1, 1, 0],
+            [1, 1, 1, 0],
+            [1, 1, 1, 0],
+            [1, 1, 1, 0],
+            [1, 1, 1, 0],
+        ])
+        model.train(data, mask)
+        result = model.output_weights[:, -1]
+        # nulled channel should also have nulled weights, since we are
+        # using batched training algorithm
+        assert_array_equal(result, np.zeros(result.shape))
 
 
 logging.basicConfig(level=logging.INFO)