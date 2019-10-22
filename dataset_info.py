55343d2f193acdb4e4226ab03d4b43af82f898d7
Max Zhao <alcasa.mz@gmail.com>
2019-10-22 20:22:42 +0200

    Updating dataset information for getting results on test and training dataset

--- dataset_info.py
+++ dataset_info.py
@@ -2,20 +2,20 @@ from flowcat import io_functions, utils
 from flowcat.utils import time_timers
 
 data = utils.URLPath("/data/flowcat-data/mll-flowdata/decCLL-9F")
-meta1 = utils.URLPath("/data/flowcat-data/mll-flowdata/decCLL-9F/case_info_2018-12-15.json")
-meta2 = utils.URLPath("/data/flowcat-data/mll-flowdata/decCLL-9F/case_info_diagnosis_2018-12-15.json.gz")
+meta1 = utils.URLPath("output/0-final/dataset/train.json.gz")
+meta2 = utils.URLPath("output/0-final/dataset/test.json.gz")
 
 with time_timers.timer("Test"):
     print("Hello")
 
 
 with time_timers.timer("DS1"):
-    dataset = io_functions.load_case_collection_from_caseinfo(data, meta1)
+    dataset = io_functions.load_case_collection(data, meta1)
 
 print(dataset)
 
 
 with time_timers.timer("DS2"):
-    dataset = io_functions.load_case_collection_from_caseinfo(data, meta2)
+    test_dataset = io_functions.load_case_collection(data, meta2)
 
-print(dataset)
+print(test_dataset)
