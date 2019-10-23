4a7adda549a780895674c56ae851341b30836940
Max Zhao <alcasa.mz@gmail.com>
2019-10-23 19:33:17 +0200

    Adding directory creation to prepare dataset script

--- 00_prepare_dataset.py
+++ 00_prepare_dataset.py
@@ -224,6 +224,7 @@ def main(data: utils.URLPath, meta: utils.URLPath, output: utils.URLPath):
     cases = io_functions.load_case_collection_from_caseinfo(data, meta)
     train, test = preprocess_cases(cases)
     reference = filter_reference(train)
+    output.mkdir()
     io_functions.save_case_collection(train, output / "train.json.gz")
     io_functions.save_case_collection(test, output / "test.json.gz")
     io_functions.save_json(reference.labels, output / "references.json")
