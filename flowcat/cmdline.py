82fc3765df190643c44b1229985b923fcb18636e
Max Zhao <alcasa.mz@gmail.com>
2019-10-24 18:53:22 +0200

    Adding transform to cmd application

--- flowcat/cmdline.py
+++ flowcat/cmdline.py
@@ -1,6 +1,8 @@
 import json
+from collections import defaultdict
 
 from flowcat import utils, io_functions, sommodels
+from flowcat.dataset import case_dataset
 from argmagic import argmagic
 
 
@@ -39,8 +41,7 @@ def reference(
         tensorboard: bool = False,
         trainargs: json.loads = None,
         selected_markers: json.loads = None):
-    """Train new reference SOM from random using the given data.
-    """
+    """Train new reference SOM from random using data filtered by labels."""
     dataset = io_functions.load_case_collection(data, meta)
     labels = io_functions.load_json(labels)
     dataset = dataset.filter(labels=labels)
@@ -81,8 +82,65 @@ def reference(
     io_functions.save_casesom(model, output)
 
 
+def transform(
+        data: utils.URLPath,
+        meta: utils.URLPath,
+        output: utils.URLPath,
+        reference: utils.URLPath,
+        transargs: json.loads = None,
+        sample: int = 0):
+    """Transform dataset using a reference SOM."""
+    dataset = io_functions.load_case_collection(data, meta)
+
+    # randomly sample 'sample' number cases from each group
+    if sample:
+        dataset = dataset.sample(sample)
+
+    if transargs is None:
+        transargs = {
+            "max_epochs": 4,
+            "batch_size": 50000,
+            "initial_radius": 4,
+            "end_radius": 1,
+        }
+
+    print(f"Loading referece from {reference}")
+    model = io_functions.load_casesom(reference, **transargs)
+
+    print(f"Trainsforming individual samples")
+    output.mkdir()
+    casesamples = defaultdict(list)
+    count_samples = len(dataset) * len(model.models)
+    countlen = len(str(count_samples))
+    for i, (case, somsample) in enumerate(utils.time_generator_logger(model.transform_generator(dataset))):
+        sompath = output / f"{case.id}_t{somsample.tube}.npy"
+        io_functions.save_som(somsample.data, sompath, save_config=False)
+        somsample.data = None
+        somsample.path = sompath
+        casesamples[case.id].append(somsample)
+        print(f"[{str(i + 1).rjust(countlen, ' ')}/{count_samples}] Created tube {somsample.tube} for {case.id}")
+
+    print(f"Saving result to new collection at {output}")
+    som_dataset = case_dataset.CaseCollection([
+        case.copy(samples=casesamples[case.id])
+        for case in dataset
+    ])
+    som_dataset.selected_markers = {
+        m.tube: m.model.markers for m in model.models.values()
+    }
+    io_functions.save_case_collection(som_dataset, output + ".json.gz")
+    io_functions.save_json(
+        {
+            tube: {
+                "dims": m.model.dims,
+                "channels": m.model.markers,
+            } for tube, m in model.models.items()
+        }, output + "_config.json")
+
+
 def train(data: utils.URLPath, output: utils.URLPath):
-    """Train a new model from the given data."""
+    """Train a new classifier using SOM data."""
+    raise NotImplementedError
 
 
 def predict(case, model: utils.URLPath, output: utils.URLPath):
@@ -93,7 +151,8 @@ def predict(case, model: utils.URLPath, output: utils.URLPath):
         model: Path to model containing CNN and SOMs.
         output: Destination for plotting.
     """
+    raise NotImplementedError
 
 
 def main():
-    argmagic([predict, train, filter, reference])
+    argmagic([predict, train, filter, reference, transform])
