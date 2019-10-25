dea9413009ae0ca7af197dd6c091fe91f4ede57e
Max Zhao <alcasa.mz@gmail.com>
2019-10-25 11:16:41 +0200

    Use consistent coloring scheme in groups for all plots

--- 00c_dataset_channel_plots.py
+++ 00c_dataset_channel_plots.py
@@ -7,11 +7,14 @@ import seaborn as sns
 sns.set()
 import matplotlib.pyplot as plt
 
-from flowcat import io_functions, utils
+from flowcat import io_functions, utils, mappings
 
 data = utils.URLPath("/data/flowcat-data/mll-flowdata/decCLL-9F")
-meta = utils.URLPath("output/0-munich-data/dataset/train.json")
-meta_test = utils.URLPath("output/0-munich-data/dataset/test.json")
+meta = utils.URLPath("/data/flowcat-data/2019-10_paper_data/0-munich-data/dataset/train.json")
+meta_test = utils.URLPath("/data/flowcat-data/2019-10_paper_data/0-munich-data/dataset/test.json")
+
+output = utils.URLPath("/data/flowcat-data/2019-10_paper_data/00c-channel-intensities")
+output.mkdir()
 
 dataset = io_functions.load_case_collection(data, meta)
 print(dataset)
@@ -26,6 +29,7 @@ def merge_cases(dataset, tube, channel):
 
 # Create channel 1D distribution plots
 groups = ["normal", "CLL", "HCL"]
+colors = mappings.ALL_GROUP_COLORS
 
 channels = [
     ("1", "CD45-KrOr"),
@@ -35,10 +39,8 @@ channels = [
 ]
 sns.set_style("white")
 
-tube, channel = channels[0]
-
 for tube, channel in channels:
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(8, 6))
     ax.set_xlim(0, 1024)
     for group in groups:
         selected = dataset.filter(groups=[group]).sample(50)
@@ -46,45 +48,45 @@ for tube, channel in channels:
             lambda d: d.data[:, d.channels.index(channel)],
             map(lambda c: c.get_tube(tube).get_data(), selected)
         )))
-        sns.kdeplot(merged, ax=ax, label=group)
+        sns.kdeplot(merged, ax=ax, label=group, color=colors[group])
 
     ax.legend()
-    ax.set_ylabel("Distribution as kernel density")
+    ax.set_ylabel("Density function")
     ax.set_xlabel("Channel intensity")
-    ax.set_title(f"{channel}")
-
-    output = utils.URLPath("output/0-munich-data/figures-dataset")
-    output.mkdir()
-    plt.savefig(str(output / f"{channel}.png"))
+    ax.set_title(f"Kernel density {channel}")
+    fig.tight_layout()
+    plt.savefig(str(output / f"{channel}.png"), dpi=300)
     plt.close("all")
 
 # Create over time distribution on a single group
 import datetime
-group = "normal"
-selection = dataset.filter(groups=[group])
-ranges = [(year, month) for year in range(2016, 2020) for month in range(1, 13)][:-11:4]
-sns.set_style("white")
-sns.set_palette(sns.cubehelix_palette(len(ranges)))
+for group, shift in [("normal", 9), ("CLL", 9), ("HCL", 4)]:
+    selection = dataset.filter(groups=[group])
+    ranges = [(year, month) for year in range(2016, 2020) for month in range(1, 13)][:-11:4]
 
-file_path = output / f"timeplot.png"
 
-fig, ax = plt.subplots()
-ax.set_xlim(0, 1024)
-for i, end_month in enumerate(ranges[1:]):
-    end = datetime.date(*end_month, 1) - datetime.timedelta(days=1)
-    start = datetime.date(*ranges[i], 1)
-    label = f"{start.strftime('%m/%y')}-{end.strftime('%m/%y')}"
-    print(label)
-    time_cases = selection.filter(date=(start, end))
-    if len(time_cases) == 0:
-        continue
-    merged = merge_cases(time_cases, "1", "CD45-KrOr")
-    sns.kdeplot(merged, ax=ax, label=label)
+    base_color = colors[group]
 
-ax.legend()
-ax.set_ylabel("Distribution as kernel density")
-ax.set_xlabel("Channel intensity")
-ax.set_title(f"Normal CD45-KrOr over time")
-fig.tight_layout()
-plt.savefig(str(file_path))
-plt.close("all")
+    color_range = [i / 20.0 for i in range(-shift, len(ranges) - shift)]
+    color_range = [[c + i for c in base_color] for i in color_range]
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.set_xlim(0, 1024)
+    for i, end_month in enumerate(ranges[1:]):
+        end = datetime.date(*end_month, 1) - datetime.timedelta(days=1)
+        start = datetime.date(*ranges[i], 1)
+        label = f"{start.strftime('%m/%y')}-{end.strftime('%m/%y')}"
+        time_cases = selection.filter(date=(start, end))
+        print(label, time_cases)
+        if len(time_cases) == 0:
+            continue
+        merged = merge_cases(time_cases, "1", "CD45-KrOr")
+        sns.kdeplot(merged, ax=ax, label=label, color=color_range[i])
+
+    ax.legend()
+    ax.set_ylabel("Density function")
+    ax.set_xlabel("Channel intensity")
+    ax.set_title(f"{group} samples CD45-KrOr over time")
+    fig.tight_layout()
+    plt.savefig(str(output / f"{group}_timeplot.png"))
+    plt.close("all")
