6c8825f991a8e6b048fe85c3de5da058b3cde29d
Max Zhao <alcasa.mz@gmail.com>
2019-10-30 12:52:25 +0100

    Dataset info on exotic datasets

--- dataset_info.py
+++ dataset_info.py
@@ -1,21 +1,10 @@
 from flowcat import io_functions, utils
 from flowcat.utils import time_timers
 
-data = utils.URLPath("/data/flowcat-data/mll-flowdata/decCLL-9F")
-meta1 = utils.URLPath("output/0-final/dataset/train.json.gz")
-meta2 = utils.URLPath("output/0-final/dataset/test.json.gz")
-
-with time_timers.timer("Test"):
-    print("Hello")
-
+data = utils.URLPath("/data/flowcat-data/2019-10_paper_data/unknown-cohorts")
+meta = utils.URLPath("/data/flowcat-data/2019-10_paper_data/unknown-cohorts/meta.json")
 
 with time_timers.timer("DS1"):
-    dataset = io_functions.load_case_collection(data, meta1)
+    dataset = io_functions.load_case_collection(data, meta)
 
 print(dataset)
-
-
-with time_timers.timer("DS2"):
-    test_dataset = io_functions.load_case_collection(data, meta2)
-
-print(test_dataset)
