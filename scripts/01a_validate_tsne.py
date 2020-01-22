77643c5f12b0b8b2d1704ba17012ea15b5174980
Max Zhao <alcasa.mz@gmail.com>
2020-01-22 18:28:51 +0100

    Update tSNE generating plot for SOMs

--- scripts/01a_validate_tsne.py
+++ scripts/01a_validate_tsne.py
@@ -14,10 +14,10 @@ import seaborn as sns
 sns.set()
 
 from argmagic import argmagic
-import flowcat
+from flowcat import utils, io_functions, mappings
 
 
-def create_tsne(data: flowcat.utils.URLPath, meta: flowcat.utils.URLPath, plotdir: flowcat.utils.URLPath):
+def create_tsne(data: utils.URLPath, meta: utils.URLPath, plotdir: utils.URLPath):
     """Generate tsne plots for a subsample of data.
 
     Args:
@@ -25,16 +25,17 @@ def create_tsne(data: flowcat.utils.URLPath, meta: flowcat.utils.URLPath, plotdi
         meta: Path to metadata json for cases.
         plotdir: Path to output plots for data.
     """
-    data = flowcat.utils.URLPath("output/test-2019-08/som")
-    meta = flowcat.utils.URLPath("output/test-2019-08/som.json")
-    plotdir = flowcat.utils.URLPath("output/test-2019-08/tsne")
-    cases = flowcat.io_functions.load_case_collection(data, meta)
+    # data = flowcat.utils.URLPath("output/test-2019-08/som")
+    # meta = flowcat.utils.URLPath("output/test-2019-08/som.json")
+    # plotdir = flowcat.utils.URLPath("output/test-2019-08/tsne")
+
+    cases = io_functions.load_case_collection(data, meta)
 
     # cases = cases.sample(20, flowcat.mappings.GROUPS)
     # flowcat.io_functions.save_json(cases.labels, plotdir / "case_ids.json")
-    labels = flowcat.io_functions.load_json(plotdir / "case_ids.json")
-    cases = cases.filter(labels=labels)
-    print(cases)
+    # labels = io_functions.load_json(plotdir / "case_ids.json")
+    # cases = cases.filter(labels=labels)
+    # print(cases)
 
     groups = np.array([case.group for case in cases])
 
@@ -50,12 +51,14 @@ def create_tsne(data: flowcat.utils.URLPath, meta: flowcat.utils.URLPath, plotdi
         "normal": "darkgoldenrod",
     }
 
+    plotdir.mkdir()
+
     for tube in ("1", "2", "3"):
         soms = []
         for case in cases:
             sample = case.get_tube(tube, kind="som")
-            sample.path = data / f"{case.id}_t{tube}.csv"
-            som = sample.get_data().data.values.flatten()
+            sample.path = data / f"{case.id}_t{tube}.npy"
+            som = sample.get_data().data.flatten()
             soms.append(som)
         somdata = np.array(soms)
 
@@ -63,7 +66,7 @@ def create_tsne(data: flowcat.utils.URLPath, meta: flowcat.utils.URLPath, plotdi
         transformed = tsne.fit_transform(somdata)
 
         fig, ax = plt.subplots(figsize=(11, 7))
-        for group in flowcat.mappings.GROUPS:
+        for group in mappings.GROUPS:
             gdata = transformed[groups == group]
             gx = gdata[:, 0]
             gy = gdata[:, 1]
